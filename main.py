a4766d11c5935d584ad09edc189d6fd37a9f2129
Islam Aymaan <eaboouf55@gmail.com>
2025-05-06 03:11:18 +0300

    Clean init with LFS tracked models

---
+++ main.py
@@ -0,0 +1,21 @@
+
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import JSONResponse
+import shutil
+import os
+
+app = FastAPI()
+
+@app.post("/predict/")
+async def predict_image(file: UploadFile = File(...)):
+    try:
+        temp_file_path = f"temp_{file.filename}"
+        with open(temp_file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        from my_model import Integrated_Model
+        result, confidence = Integrated_Model(temp_file_path)
+        os.remove(temp_file_path)
+        return JSONResponse(content={"result": result, "confidence": confidence})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
